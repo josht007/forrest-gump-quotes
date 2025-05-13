5e5a1adbb41add87b762c0bce003dc0bcc0c5011
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-12 23:01:09 -0400

    Initial commit

---
+++ src/main/java/com/josh007/forrestgumpquote/ForrestGumpQuoteApplication.java
@@ -0,0 +1,13 @@
+package com.josh007.forrestgumpquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForrestGumpQuoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForrestGumpQuoteApplication.class, args);
+    }
+
+}
