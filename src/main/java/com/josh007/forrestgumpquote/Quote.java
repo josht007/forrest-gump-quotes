06180485b8bd1f67af0d3b6814353a88890fc178
josht007 <148504666+josht007@users.noreply.github.com.>
2025-05-16 23:57:47 -0400

    Added Quote class implementations

--- src/main/java/com/josh007/forrestgumpquote/Quote.java
+++ src/main/java/com/josh007/forrestgumpquote/Quote.java
@@ -2,5 +2,9 @@ package com.josh007.forrestgumpquote;
 
 
 //Write the class for the Quote class
-public class Quote {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Quote(String type, Value value) {
+
 }
